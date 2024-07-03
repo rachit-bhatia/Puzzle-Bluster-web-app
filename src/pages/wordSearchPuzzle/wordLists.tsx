70fc723a27d323097a07b3ef23dbc8de7db0c6c6
Rachit Bhatia <rachit.bhatia@configura.com>
2024-07-04 01:17:00 +0800

    refactor code to remove redundancy

---
+++ src/pages/wordSearchPuzzle/wordLists.tsx
@@ -0,0 +1,53 @@
+const wordsToFindEasy: String[] = [
+    "APPLE", 
+    "MANGO", 
+    "MELON", 
+    "BREAD", 
+    "CRANE", 
+    "DREAM", 
+    "EARTH", 
+    "FLUTE", 
+    "GHOST", 
+    "HOUSE", 
+    "LIGHT", 
+    "OCEAN"
+];
+
+
+const wordsToFindMedium: string[] = [
+    "HELLO",
+    "WORLD",
+    "REACT",
+    "JAVA",
+    "COMPONENT",
+    "FUNCTION",
+    "DEVELOP",
+    "PROGRAM",
+    "FRONTEND",
+    "BACKEND",
+    "DATABASE",
+    "SERVER",
+    "CLIENT",
+    "FETCH",
+    "STATE",
+];
+
+const wordsToFindHard: String[] = [
+    "BREEZE", 
+    "MYSTERY", 
+    "CRYSTAL", 
+    "VOLCANO", 
+    "JOURNEY", 
+    "HORIZON", 
+    "ENIGMA", 
+    "FESTIVAL", 
+    "HARMONY", 
+    "SUNSHINE", 
+    "WHISPER", 
+    "SYMPHONY", 
+    "DESTINY", 
+    "ETERNITY", 
+    "PARADISE"
+];
+
+export { wordsToFindEasy, wordsToFindMedium, wordsToFindHard };
