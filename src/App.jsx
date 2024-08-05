3c208293e21e5bd84a4e9c444d1228f797664b72
jwon0164 <jwon0164@student.monash.edu>
2024-08-05 13:14:03 +0800

    UT9-2 UI implementation for puzzle selection to test routing and add math game picture

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,33 @@
-import { ReactElement, useState } from 'react'
+import { ReactElement, useState } from "react";
 // import './App.css'
-import SignInPage from './pages/signIn/sign-in'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import SignUpPage from './pages/signUp/sign-up'
-import HomePage from './pages/home/home'
-import AccountDetails from './pages/accountDetails/account-details'
-import DifficultySelectionPage from './pages/difficultySelection/difficultySelection'
-import RenderPuzzle from './pages/wordSearchPuzzle/renderPuzzle'
-import React from 'react'
-
+import SignInPage from "./pages/signIn/sign-in";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUpPage from "./pages/signUp/sign-up";
+import HomePage from "./pages/home/home";
+import AccountDetails from "./pages/accountDetails/account-details";
+import DifficultySelectionPage from "./pages/difficultySelection/difficultySelection";
+import PuzzleSelectionPage from "./pages/puzzleSelection/puzzleSelection";
+import RenderPuzzle from "./pages/wordSearchPuzzle/renderPuzzle";
+import React from "react";
 
 function App() {
-
-
   return (
-
-    
     <BrowserRouter>
       <Routes>
-          <Route index element={<SignInPage/>} /> 
-          <Route path="/home" element={<HomePage/>} />
-          <Route path="/signin" element={<SignInPage/>} />
-          <Route path="/signup" element={<SignUpPage/>} />
-          <Route path="/account-details" element={<AccountDetails/>} />
-          <Route path="/difficultyselection" element={<DifficultySelectionPage/>} />
-          <Route path="/render/:difficulty" element={<RenderPuzzle/>} />
+        <Route index element={<SignInPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/account-details" element={<AccountDetails />} />
+        <Route
+          path="/difficultyselection"
+          element={<DifficultySelectionPage />}
+        />
+        <Route path="/puzzleSelection" element={<PuzzleSelectionPage />} />
+        <Route path="/render/:difficulty" element={<RenderPuzzle />} />
       </Routes>
     </BrowserRouter>
-
-       
-
-
-    
-
-    
-  )
+  );
 }
 
-export default App
+export default App;
