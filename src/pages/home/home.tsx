acaeb91943f7e3d1fb60de33656d94c13c597d76
jwon0164 <jwon0164@student.monash.edu>
2024-08-08 10:44:37 +0800

    Added puzzle selection page in home.tsx

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -5,44 +5,41 @@ import { Navigate, Link, useNavigate } from "react-router-dom";
 import { UserAccount } from "../../models/shared";
 
 function HomePage() {
-
-  const [errorMessage, setErrorMessage] = useState('');
-  const [isSignOutSuccessful,setIsSignOutSuccessful] = useState(false)
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isSignOutSuccessful, setIsSignOutSuccessful] = useState(false);
   const navigate = useNavigate();
 
   const onSignOut = async (event) => {
     event.preventDefault();
     try {
-      await auth.signOut()
-      console.log("sign out successful")
-      setIsSignOutSuccessful(true)
-    }
-    catch(error){
-      setErrorMessage(errorMessage)
-      console.log(error)
+      await auth.signOut();
+      console.log("sign out successful");
+      setIsSignOutSuccessful(true);
+    } catch (error) {
+      setErrorMessage(errorMessage);
+      console.log(error);
     }
   };
-}
 
-// REMOVE THIS ( JUST FOR TESTING)
-const getUsers = async (event) => {
-  event.preventDefault();
+  // REMOVE THIS ( JUST FOR TESTING)
+  const getUsers = async (event) => {
+    event.preventDefault();
 
-    await UserAccount.getCollection()
-    console.log(UserAccount.users)
-    
-  }
+    await UserAccount.getCollection();
+    console.log(UserAccount.users);
+  };
   return (
     <div>
-      {isSignOutSuccessful &&  <Navigate to="/signin" replace={true} /> }
+      {isSignOutSuccessful && <Navigate to="/signin" replace={true} />}
       <h1>Puzzle Bluster</h1>
       <h3>Let's solve some Word Search Puzzles!</h3>
-      <div style = {{paddingTop: "100px"}}>
-      <button onClick={onSignOut}>Sign Out</button>
-      <button onClick={() => navigate("/account-details")}>Account Details</button>
-      <button onClick={() => navigate("/difficultyselection")}>Start Game</button>
+      <div style={{ paddingTop: "100px" }}>
+        <button onClick={onSignOut}>Sign Out</button>
+        <button onClick={() => navigate("/account-details")}>
+          Account Details
+        </button>
+        <button onClick={() => navigate("/puzzleselection")}>Start Game</button>
       </div>
-
     </div>
   );
 }
