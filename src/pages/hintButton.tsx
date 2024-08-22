d5fa668c585a088a87b9da12f55dcf1066fd636a
Rachit Bhatia <rachit.bhatia@configura.com>
2024-08-23 01:28:10 +0800

    UT4-2 add button to for hints

---
+++ src/pages/hintButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const HintButton = () => {
+  return (
+    <button style={{marginRight: '20px'}}>
+      Show Hint
+    </button>
+  );
+};
+
+export default HintButton;
--- src/pages/wordSearchPuzzle/levelIndicator.tsx
+++ src/pages/wordSearchPuzzle/levelIndicator.tsx
@@ -7,9 +7,8 @@ interface LevelIndicatorProps {
   
   const LevelIndicator: React.FC<LevelIndicatorProps> = ({ level }) => {
     const indicatorStyle: React.CSSProperties = {
-      position: 'absolute',
-      top: '10px',
-      right: '10px',
+      display: 'flex',
+      alignItems: 'center',
       backgroundColor: 'rgba(53, 42, 27, 0.6)',
       padding: '5px 10px',
       borderRadius: '5px',
